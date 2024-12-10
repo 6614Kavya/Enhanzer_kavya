4ef7248e1d43c0cb1687245b9cd476cc1d8b0d0d
6614Kavya <109873890+6614Kavya@users.noreply.github.com>
2024-12-10 10:20:14 +0530

    created the book list

--- book_store/src/app/app.component.ts
+++ book_store/src/app/app.component.ts
@@ -6,8 +6,20 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'book_store';
+
+  books = [
+    { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', year: 1925 },
+    { title: 'To Kill a Mockingbird', author: 'Harper Lee', year: 1960 },
+    { title: '1984', author: 'George Orwell', year: 1949 },
+    { title: 'Moby-Dick', author: 'Herman Melville', year: 1851 },
+    { title: 'Pride and Prejudice', author: 'Jane Austen', year: 1813 },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
